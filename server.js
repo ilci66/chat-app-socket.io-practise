e5136535459bf03a2e3ff905863f9d5288e37449
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-29 12:33:43 +0200

    initial commit

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ console.log(process.env.PORT)
 
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+//serving the static is very important and I keep forgetting;
+app.use(express.static(path.join(__dirname, public)));
+
+app.listen(port, () => console.log(`app is live on port: ${port}`))
